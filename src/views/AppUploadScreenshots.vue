f9d328dce00a49cc16f7614c7f12dc443806a6fb
Chiang <allanchiang@ch-si.com.tw>
2022-09-27 16:27:21 +0800

    add uploadScreenShots function

--- src/views/AppUploadScreenshots.vue
+++ src/views/AppUploadScreenshots.vue
@@ -1,28 +1,39 @@
 <template>
   <div class="container">
     <h1 class="title">AppStore - Upload Screenshots</h1>
-    <Swiper
-      class="mySwiper"
-      :slides-per-view = "3"
-      :height = "100"
-      :space-between = "24"
-    >
-      <SwiperSlide
-        class="mySwiperSlide"
-        v-for="photo in photos" :key="photo"
+    <div class="screenshots">
+      <div class="screenshots-header">
+        <p>Upload Screenshots</p>
+        <p>{{`${photos.length} / 6`}}</p>
+      </div>
+      <Swiper
+        class="mySwiper"
+        :slides-per-view = "3"
+        :height = "100"
+        :space-between = "24"
       >
-        <img :src="photo" alt="screenshots">
-      </SwiperSlide>
-    </Swiper>
+        <SwiperSlide
+          class="mySwiperSlide"
+          v-for="photo in photos" :key="photo"
+        >
+          <img :src="photo.locationPath" alt="screenshots">
+        </SwiperSlide>
+      </Swiper>
+      <label for="uploadScreenShots" class="uploadScreen">
+        <input type="file"
+              accept="image/*"
+              id="uploadScreenShots"
+              ref="uploadScreen"
+              @change="uploadScreenShots"
+        />
+        <span>+</span>
+      </label>
+    </div>
   </div>
 </template>
 
 <script>
 import { Swiper, SwiperSlide } from 'swiper/vue';
-import photo1 from '../assets/images/Test1.jpg';
-import photo2 from '../assets/images/Test2.jpg';
-import photo3 from '../assets/images/Test3.jpg';
-import photo4 from '../assets/images/Test4.jpg';
 
 export default {
   components: {
@@ -31,10 +42,24 @@ export default {
   },
   data() {
     return {
-      photos: [
-        photo1, photo2, photo3, photo4,
-      ],
+      photos: [],
     };
+  },
+  methods: {
+    uploadScreenShots() {
+      if (this.photos.length < 6) {
+        const screenShot = this.$refs.uploadScreen.files[0];
+        const screenPath = URL.createObjectURL(screenShot);
+        this.photos.push({
+          locationPath: screenPath,
+        });
+      } else {
+        this.$swal.fire({
+          icon: 'warning',
+          text: '已達上傳檔案上限 6 張',
+        });
+      }
+    },
   },
 };
 </script>
