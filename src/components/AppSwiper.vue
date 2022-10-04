65ae251056d520bf2c48f68c0da04c7cbfd9379e
Chiang <allanchiang@ch-si.com.tw>
2022-10-04 17:11:28 +0800

    add upload Screenshot device

--- src/components/AppSwiper.vue
+++ src/components/AppSwiper.vue
@@ -5,23 +5,25 @@
       <p>{{ `${photos.length} / 6` }}</p>
     </div>
     <ul class="slide-body" ref="slideBody">
-      <li v-for="(item, index) in photos" :key="item.locationPath"
-          class="slide-item"
-        >
-        <img :src="item.locationPath" alt=""
-            :style="{width: `${slideWidth}px`, height: `${slideHeight}px`}"
-        >
+      <li v-for="(item, index) in photos" :key="item.locationPath" class="slide-item">
+        <img
+          :src="item.locationPath"
+          alt=""
+          :style="{ width: `${slideWidth}px`, height: `${slideHeight}px` }"
+        />
         <button @click="deleteImg(index)">X</button>
       </li>
     </ul>
     <!-- 上傳按鈕 -->
     <label for="upload" class="uploadBtn">
-      <input type="file" id="upload"
+      <input
+        type="file"
+        id="upload"
         accept="image/*"
         multiple
         ref="uploadScreenshot"
         @change="uploadScreenshot"
-      >
+      />
       <span>+</span>
     </label>
   </div>
@@ -55,22 +57,30 @@ export default {
     // 上傳
     uploadScreenshot() {
       const screenImgs = Array.from(this.$refs.uploadScreenshot.files);
-      if (screenImgs.length > 0) {
-        screenImgs.forEach((item) => {
-          this.photos.push({
-            locationPath: URL.createObjectURL(item),
-          });
-        });
-      } else {
-        this.$swal.fire({
-          icon: 'warning',
-          text: '未上傳任何圖檔',
-        });
+      if (this.photos.length >= 6) {
+        this.sweetAlert('warning', '圖片空間已達上限 6 張');
+        return;
       }
+      if (screenImgs.length > 6) {
+        this.sweetAlert('warning', '上傳數量超過 6 張');
+        return;
+      }
+      screenImgs.forEach((item) => {
+        this.photos.push({
+          locationPath: URL.createObjectURL(item),
+        });
+      });
     },
     // 移除截圖
     deleteImg(index) {
       this.photos.splice(index, 1);
+    },
+    // alert
+    sweetAlert(icon, msg) {
+      this.$swal.fire({
+        icon,
+        text: msg,
+      });
     },
   },
 };
