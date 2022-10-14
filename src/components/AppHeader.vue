aa425258e3cc11157920904602e4b47a2ca89835
Chiang <allanchiang@ch-si.com.tw>
2022-10-14 16:23:15 +0800

    add dark mode switch

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,7 +5,35 @@
       <router-link to="/UploadFile">UploadFile</router-link>
       <router-link to="/UploadScreenshots">Screenshots</router-link>
       <router-link to="/AppDrag">Drag</router-link>
-      <router-link to="/Test">Test</router-link>
+      <router-link to="/Test">Transition</router-link>
     </nav>
+    <label for="darkMode" :class="['switch', {'dark': isDark }]">
+      <img src="../assets/icon/light_mode.svg" alt="light" class="switch-icon">
+      <img src="../assets/icon/dark_mode.svg" alt="dark" class="switch-icon">
+      <input
+        type="checkbox"
+        name="darkMode"
+        id="darkMode"
+        v-model="isDark"
+        @click="changeMode"
+        hidden
+      >
+      <div class="switch-btn"></div>
+    </label>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      isDark: false,
+    };
+  },
+  methods: {
+    changeMode() {
+      console.log(this.isDark);
+    },
+  },
+};
+</script>
--- src/views/AppTest.vue
+++ src/views/AppTest.vue
@@ -1,16 +1,65 @@
 <template>
   <div class="container">
-    <h1 class="title">Code Test</h1>
+    <h1 class="title">Vue Transition Test</h1>
 
-    <transition-group class="testList" name="test" tag="ul">
-      <li v-for="(item, index) in list"
-          :key="item"
-          class="testList-item"
-          draggable="true"
-          @dragstart="dragstart(index)"
-          @dragenter="dragenter(index)"
-      >{{ item }}</li>
-    </transition-group>
+    <div class="row">
+      <!-- Transition 正常類別 + Draggable -->
+      <div class="col-md-4">
+        <h2 class="subTitle">TransitionGroup 正常類別 + Draggable</h2>
+        <transition-group class="testList" name="test" tag="ul">
+          <li v-for="(item, index) in list"
+              :key="item"
+              class="testList-item"
+              draggable="true"
+              @dragstart="dragstart(index)"
+              @dragenter="dragenter(index)"
+          >{{ item }}</li>
+        </transition-group>
+      </div>
+      <!-- Transition 正常類別 -->
+      <div class="col-md-4">
+        <h2 class="subTitle">Transition 正常類別</h2>
+        <button @click="transitionTest" class="btnStyle">Toggle</button>
+        <p>Transition</p>
+        <transition name="fade">
+          <div v-if="isShow" class="box"></div>
+        </transition>
+        <p>Animation</p>
+        <transition name="bounce">
+          <div v-if="isShow" class="box"></div>
+        </transition>
+      </div>
+      <!-- Transition 自訂義類別 -->
+      <div class="col-md-4">
+        <h2 class="subTitle">Transition 自訂義類別</h2>
+        <button @click="transitionTest2" class="btnStyle">Toggle</button>
+        <transition
+          enter-from-class="opacity-0"
+          enter-active-class="trs-time-3"
+          leave-active-class="trs-time-3"
+          leave-to-class="opacity-0"
+        >
+          <div v-if="isShow2" class="box"></div>
+        </transition>
+      </div>
+      <!-- Transition 套用 Animate.css 套件 -->
+      <div class="col-md-4">
+        <h2 class="subTitle">Transition 套用 Animate.css 套件</h2>
+        <button @click="transitionTest3" class="btnStyle">Toggle</button>
+        <transition
+          enter-active-class="animate__animated animate__tada"
+          leave-active-class="animate__animated animate__backOutDown"
+        >
+          <div v-if="isShow3" class="box"></div>
+        </transition>
+      </div>
+      <!-- : class test -->
+      <div class="col-md-4">
+        <h2 class="subTitle">:class Test</h2>
+        <button class="btnStyle" @click="changeBg">Change</button>
+        <div :class="['box', {'darkBox': darkModa}]"></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -22,9 +71,12 @@ export default {
     return {
       test: 'Test',
       isShow: true,
+      isShow2: true,
+      isShow3: true,
       list: ['1', '2', '3', '4', '5'],
       location: [],
       dragIndex: '',
+      darkModa: false,
     };
   },
   methods: {
@@ -46,6 +98,18 @@ export default {
         this.list.splice(index, 0, move);
         this.dragIndex = index;
       }
+    },
+    transitionTest() {
+      this.isShow = !this.isShow;
+    },
+    transitionTest2() {
+      this.isShow2 = !this.isShow2;
+    },
+    transitionTest3() {
+      this.isShow3 = !this.isShow3;
+    },
+    changeBg() {
+      this.darkModa = !this.darkModa;
     },
   },
 };
